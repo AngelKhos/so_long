6a223732adf81363bea093731511b7dffd6c491e
BDE <bde@MacBook-Pro-de-BDE.local>
2025-03-29 17:17:43 +0100

    ignore previous commit

--- .deps/img.d
+++
@@ -1,8 +0,0 @@
-.objs/img.o: srcs/img.c srcs/../incs/so_long.h \
-  srcs/../incs/../libft/libft.h srcs/../incs/../minilibx/mlx.h
-
-srcs/../incs/so_long.h:
-
-srcs/../incs/../libft/libft.h:
-
-srcs/../incs/../minilibx/mlx.h:
--- .deps/map_checking.d
+++
@@ -1,8 +0,0 @@
-.objs/map_checking.o: srcs/map_checking.c srcs/../incs/so_long.h \
-  srcs/../incs/../libft/libft.h srcs/../incs/../minilibx/mlx.h
-
-srcs/../incs/so_long.h:
-
-srcs/../incs/../libft/libft.h:
-
-srcs/../incs/../minilibx/mlx.h:
